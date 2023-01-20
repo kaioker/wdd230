7cca8b493b4ab21864e456875c75cff272647676
Blake (Kaioker) <kaioker2@gmail.com>
2023-01-20 06:44:05 +0000

    fsa

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,7 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("modified").innerHTML = document.lastModified;
+
+var views = localStorage.getItem('views') || 0;
+views++;
+document.getElementById("views").innerHTML = "You have visited this page " + views + " time" + (views == 1 ? '' : 's');
+localStorage.setItem('views', views);
