202f6be64420d69e7597059ec59115acb7e28fee
Blake (Kaioker) <kaioker2@gmail.com>
2023-01-22 05:32:29 +0000

    meh.

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,3 +1,12 @@
 document.getElementById("menubutton").addEventListener("click", function(){
     document.getElementById("mainnav").classList.toggle("responsive");
 }, false);
+if(document.getElementById("time")){
+    var lastVisit = localStorage.getItem("lastVisit") || new Date().getTime();
+    var thisVisit = new Date().getTime();
+    localStorage.setItem("lastVisit", thisVisit);
+    var timeDiff = Math.abs(thisVisit - lastVisit);
+    console.log(timeDiff);
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    document.getElementById("time").innerHTML = "You last visited this page " + diffDays + " day" + (diffDays == 1 ? "" : "s") + " ago.";
+}
