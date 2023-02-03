dc9120aecada0dcfc2636e5cc8e0a04d651a6a7c
Blake (Kaioker) <kaioker2@gmail.com>
2023-02-03 05:09:46 +0000

    meh.

--- scripts/menu.js
+++ scripts/menu.js
@@ -76,3 +76,61 @@ document.getElementById("form").addEventListener("submit", function(e){
 
     document.getElementById("form").reset();
 });
+
+var requestURL = 'https://byui.projectpine.com/wdd230/menu.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    displayActivities(request.response);
+}
+function displayActivities(activities){
+    var activitybox = document.getElementById("activities");
+
+    for(i=0;i<Object.keys(activities).length;i++){
+        var li = document.createElement("li");
+        var items = activities[Object.keys(activities)[i]];
+        var span = document.createElement("span");
+        span.innerHTML = Object.keys(activities)[i]+": ";
+        li.appendChild(span);
+        for(j=0;j<Object.keys(items).length;j++){
+            if(j != 0){
+                var span2 = document.createElement("span");
+                span2.innerHTML = " | ";
+                li.appendChild(span2);
+            }
+            var a = document.createElement("a");
+            a.href = items[Object.keys(items)[j]];
+            a.innerHTML = Object.keys(items)[j];
+            if(!a.href.includes("javascript:")) a.target = "_blank"; //if it's not a javascript link, open in new tab
+            li.appendChild(a);
+        }
+        activitybox.appendChild(li);
+    }
+}
+
+const url = 'https://byui.projectpine.com/wdd230/api_proxy.php?q=London,GB&units=metric';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+        const data = await response.json();
+        doAThing(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function doAThing(jsObject) {
+    console.log(jsObject);
+    document.getElementById('icon').src = "https://openweathermap.org/img/w/"+jsObject.weather[0].icon+".png";
+    document.getElementById('temp').textContent = "Feels Like: "+jsObject.main.feels_like+ "°C";
+    document.getElementById('condition').textContent = jsObject.weather[0].description;
+}
---
+++ week05/scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// i wrapped the api in a php proxy to add some basic security and hide my api key.
+// forgive me for not using the api key in the url like the instructions said but i felt like this was a better way to do it.
+// even if it is beyond the scope of this class.
+const url = 'https://byui.projectpine.com/wdd230/api_proxy.php?q=trier,de&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+        const data = await response.json();
+        display(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function display(data) {
+    currentTemp.innerHTML = data.main.temp+"&deg;F"; 
+    const iconsrc = "https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
